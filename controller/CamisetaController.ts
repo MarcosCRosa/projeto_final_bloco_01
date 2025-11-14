32921ca700bdfedc1a8efa0d6bf4225fd45d78b6
MarcosCRosa <marcosluiz4012@gmail.com>
2025-11-14 12:50:28 -0300

    feat:criado Collections e os metodos adicionar e listar ate o momento.

--- controller/CamisetaController.ts
+++ controller/CamisetaController.ts
@@ -1,12 +1,25 @@
+import { Produto } from "../model/Produto";
 import { Camiseta } from "../model/ProdutoCamiseta";
 import { ProdutoRepository } from "../repository/ProdutoRepository";
 
 class CamisetaController implements ProdutoRepository{
+
+    private listaCamisetas: Array<Camiseta> = new Array<Camiseta>();
+    private numeroID:number=0;
     adicionar(Produto: Camiseta): void {
-        throw new Error("Method not implemented.");
+        this.listaCamisetas.push(Produto);
+        console.log(`Camiseta:${Produto.nome},Adicionada!`);
+        
     }
     listar(): void {
-        throw new Error("Method not implemented.");
+        if(this.listaCamisetas.length===0){
+            console.log("\n O carrinho esta vazio!");
+            return;
+        }
+        console.log("Lista de Camisetas Adicionadas:");
+        for(let Camiseta of this.listaCamisetas){
+            Camiseta.visualizar();
+        }
     }
     buscarPorId(id: number): void {
         throw new Error("Method not implemented.");
